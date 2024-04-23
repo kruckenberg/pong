a68c5d2e0cb953d5c9111385ab306a7da2158a32
Nick Kruckenberg <nkruckenberg@gmail.com>
2024-04-23 19:04:01 +0000

    basic game loop

--- pong.py
+++ pong.py
@@ -8,8 +8,10 @@ WINDOW_HEIGHT = 600
 
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
+clock = pygame.time.Clock()
 
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
